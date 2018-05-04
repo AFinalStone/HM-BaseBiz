978d439622c7f3191ff70e2ccc5e51d4daa79624
houjinyun <houjinyun@gmail.com>
2018-05-04 09:56:45 +0800

    增加处理新老协议结果的处理方法

--- library/src/main/java/com/hm/iou/base/utils/RxUtil.java
+++ library/src/main/java/com/hm/iou/base/utils/RxUtil.java
@@ -21,6 +21,25 @@ public class RxUtil {
         return new Function<BaseResponse<T>, T>() {
             @Override
             public T apply(BaseResponse<T> response) throws Exception {
+                if (response.getErrorCode() == 0) {
+                    return response.getData();
+                } else {
+                    throw new ApiException("" + response.getErrorCode(), response.getMessage());
+                }
+            }
+        };
+    }
+
+    /**
+     * 统一处理请求结果，为了兼容以前老的接口协议，需要同时判断success和code值
+     *
+     * @param <T>
+     * @return
+     */
+    public static <T> Function<BaseResponse<T>, T> handleResponseEx() {
+        return new Function<BaseResponse<T>, T>() {
+            @Override
+            public T apply(BaseResponse<T> response) throws Exception {
                 if (response.isSuccess() && response.getErrorCode() == 0) {
                     return response.getData();
                 } else {
@@ -29,4 +48,5 @@ public class RxUtil {
             }
         };
     }
+
 }
