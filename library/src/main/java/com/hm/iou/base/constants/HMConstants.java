16dfb98960437a66dfe67b0f062161c95545d4f5
houjinyun <houjinyun@gmail.com>
2018-05-24 14:46:28 +0800

    增加手机号、邮箱正则

--- library/src/main/java/com/hm/iou/base/constants/HMConstants.java
+++ library/src/main/java/com/hm/iou/base/constants/HMConstants.java
@@ -8,4 +8,8 @@ public class HMConstants {
 
     public static final String USER_NOT_LOGIN = "9995";//用户未登录
 
+    public static final String REG_MOBILE = "^[1][0-9]{10}$";
+
+    public static final String REG_EMAIL_NUMBER = "^([a-z0-9A-Z]+[-|\\.]?)+[a-z0-9A-Z]@([a-z0-9A-Z]+(-[a-z0-9A-Z]+)?\\.)+[a-zA-Z]{2,}$";
+
 }
