29158822fffe6958895703aa527db770cc8effe6
houjinyun <houjinyun@gmail.com>
2018-05-28 09:33:20 +0800

    修改邮箱正则

--- library/src/main/java/com/hm/iou/base/constants/HMConstants.java
+++ library/src/main/java/com/hm/iou/base/constants/HMConstants.java
@@ -10,6 +10,6 @@ public class HMConstants {
 
     public static final String REG_MOBILE = "^[1][0-9]{10}$";
 
-    public static final String REG_EMAIL_NUMBER = "^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+[\\.][a-zA-Z0-9_-]+$";
+    public static final String REG_EMAIL_NUMBER = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]+$";
 
 }
