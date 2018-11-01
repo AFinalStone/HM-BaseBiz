c72216c32f630163b5aaa21675897943b4153dc4
石磊 <syl@shileideMacBook-Pro.local>
2018-11-01 13:57:47 +0800

    修改fileUtil工具类

--- library/src/main/java/com/hm/iou/base/file/FileUtil.java
+++ library/src/main/java/com/hm/iou/base/file/FileUtil.java
@@ -5,13 +5,11 @@ import android.app.Activity;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Environment;
 import android.os.Handler;
 import android.os.Looper;
-import android.os.Process;
 import android.support.annotation.NonNull;
 import android.widget.Toast;
 
@@ -96,65 +94,6 @@ public class FileUtil {
     }
 
     /**
-     * 获取缓存文件的根目录
-     * <p>
-     * getCacheDir()方法用于获取/data/data/<application package>/cache目录
-     * getFilesDir()方法用于获取/data/data/<application package>/files目录
-     * <p>
-     * 应用程序在运行的过程中如果需要向手机上保存数据，一般是把数据保存在SDcard中的。
-     * 大部分应用是直接在SDCard的根目录下创建一个文件夹，然后把数据保存在该文件夹中。
-     * 这样当该应用被卸载后，这些数据还保留在SDCard中，留下了垃圾数据。
-     * 如果你想让你的应用被卸载后，与该应用相关的数据也清除掉，该怎么办呢？
-     * 通过Context.getExternalFilesDir()方法可以获取到 SDCard/Android/data/你的应用的包名/files/ 目录，一般放一些长时间保存的数据
-     * 通过Context.getExternalCacheDir()方法可以获取到 SDCard/Android/data/你的应用包名/cache/目录，一般存放临时缓存数据
-     * 如果使用上面的方法，当你的应用在被用户卸载后，SDCard/Android/data/你的应用的包名/ 这个目录下的所有文件都会被删除，不会留下垃圾信息。
-     * <p>
-     * <p>
-     * 获取外部SD卡的缓存路径，需要访问外部存储设备的权限，如果外部Cache获取失败，会尝试获取内部存储设备的cache
-     *
-     * @param context
-     * @return
-     */
-    public static String getExternalCacheDirPath(@NonNull Context context) {
-        if (context == null) {
-            return null;
-        }
-        String cachePath = null;
-        int granted = context.checkPermission(Manifest.permission.READ_EXTERNAL_STORAGE, android.os.Process.myPid(), Process.myUid());
-        if (PackageManager.PERMISSION_GRANTED == granted && hasSdcard()) {
-            File file = context.getExternalCacheDir();
-            if (file != null) {
-                cachePath = file.getAbsolutePath();
-            }
-        }
-        if (cachePath == null) {
-            cachePath = getCacheDirPath(context);
-        }
-        return cachePath;
-    }
-
-    /**
-     * 获取APP本身的缓存路径
-     *
-     * @param context
-     * @return
-     */
-    public static String getCacheDirPath(@NonNull Context context) {
-        return context.getCacheDir().getAbsolutePath();
-    }
-
-
-    /**
-     * 判断是否存在SD卡
-     *
-     * @return
-     */
-    private static boolean hasSdcard() {
-        String state = Environment.getExternalStorageState();
-        return Environment.MEDIA_MOUNTED.equals(state) || !Environment.isExternalStorageRemovable();
-    }
-
-    /**
      * 下载图片
      *
      * @param context
@@ -212,5 +151,61 @@ public class FileUtil {
             }
         });
     }
+
+    /**
+     * 获取缓存文件的根目录
+     * <p>
+     * getCacheDir()方法用于获取/data/data/<application package>/cache目录
+     * getFilesDir()方法用于获取/data/data/<application package>/files目录
+     * <p>
+     * 应用程序在运行的过程中如果需要向手机上保存数据，一般是把数据保存在SDcard中的。
+     * 大部分应用是直接在SDCard的根目录下创建一个文件夹，然后把数据保存在该文件夹中。
+     * 这样当该应用被卸载后，这些数据还保留在SDCard中，留下了垃圾数据。
+     * 如果你想让你的应用被卸载后，与该应用相关的数据也清除掉，该怎么办呢？
+     * 通过Context.getExternalFilesDir()方法可以获取到 SDCard/Android/data/你的应用的包名/files/ 目录，一般放一些长时间保存的数据
+     * 通过Context.getExternalCacheDir()方法可以获取到 SDCard/Android/data/你的应用包名/cache/目录，一般存放临时缓存数据
+     * 如果使用上面的方法，当你的应用在被用户卸载后，SDCard/Android/data/你的应用的包名/ 这个目录下的所有文件都会被删除，不会留下垃圾信息。
+     * <p>
+     * <p>
+     * 获取外部SD卡的缓存路径，需要访问外部存储设备的权限，如果外部Cache获取失败，会尝试获取内部存储设备的cache
+     *
+     * @param context
+     * @return
+     */
+    public static String getExternalCacheDirPath(@NonNull Context context) {
+        String cachePath = null;
+        if (hasSdcard()) {
+            File file = context.getExternalCacheDir();
+            if (file != null) {
+                cachePath = file.getAbsolutePath();
+            }
+        }
+        if (cachePath == null) {
+            cachePath = getCacheDirPath(context);
+        }
+        return cachePath;
+    }
+
+    /**
+     * 获取APP本身的缓存路径
+     *
+     * @param context
+     * @return
+     */
+    private static String getCacheDirPath(@NonNull Context context) {
+        return context.getCacheDir().getAbsolutePath();
+    }
+
+
+    /**
+     * 判断是否存在SD卡
+     *
+     * @return
+     */
+    private static boolean hasSdcard() {
+        String state = Environment.getExternalStorageState();
+        return Environment.MEDIA_MOUNTED.equals(state) || !Environment.isExternalStorageRemovable();
+    }
+
 
 }
