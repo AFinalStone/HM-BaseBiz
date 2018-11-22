48647bf27eb95872c688c5ed7c652d43777a90e8
houjinyun <houjinyun@gmail.com>
2018-11-22 17:06:12 +0800

    增加上传文件类型

--- library/src/main/java/com/hm/iou/base/file/FileBizType.java
+++ library/src/main/java/com/hm/iou/base/file/FileBizType.java
@@ -40,7 +40,8 @@ public enum FileBizType {
     CustomerPrivacyFile(100, "用户私密文件"),//ps: 该类型的会使用私密的bucket存储用户的文件
     IdCardFront(101, "身份证正面"),//ps: 该类型的会使用私密的bucket存储用户的文件
     IdCardBack(102, "身份证反面"),//ps: 该类型的会使用私密的bucket存储用户的文件
-    LivenessPic(103, "用户活体图片");//ps: 该类型的会使用私密的bucket存储用户的文件
+    LivenessPic(103, "用户活体图片"),//ps: 该类型的会使用私密的bucket存储用户的文件
+    LivenessEncrypted(105, "活体加密数据");
 
     int type;
     String name;
