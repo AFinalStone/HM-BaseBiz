8fff6e025bffdaee574a3fe40a9c88789ce5188e
石磊 <syl@shileideMacBook-Pro.local>
2019-03-19 19:21:54 +0800

    imageloadUtil工具类对图片路径进行非空判断

--- library/src/main/java/com/hm/iou/base/utils/ImageLoadUtil.java
+++ library/src/main/java/com/hm/iou/base/utils/ImageLoadUtil.java
@@ -2,11 +2,7 @@ package com.hm.iou.base.utils;
 
 import android.content.Context;
 import android.text.TextUtils;
-import android.widget.ImageView;
 
-import com.hm.iou.base.R;
-import com.hm.iou.router.Router;
-import com.hm.iou.tools.ImageLoader;
 import com.hm.iou.tools.SystemUtil;
 
 /**
@@ -23,7 +19,7 @@ public class ImageLoadUtil {
      * @param imageUrl 图片链接
      */
     public static String getImageRealUrl(Context context, String imageUrl) {
-        if (imageUrl.startsWith("hmiou://")) {
+        if (!TextUtils.isEmpty(imageUrl) && imageUrl.startsWith("hmiou://")) {
             String placeString = String.format("android.resource://%s/", SystemUtil.getCurrentAppPackageName(context));
             imageUrl = imageUrl.replace("hmiou://", placeString);
         }
