2e201434b72c802132ccfd420fc8e5520e21d4f1
石磊 <>
2019-05-14 16:24:32 +0800

    添加HMTextChangeListener对象

---
+++ library/src/main/java/com/hm/iou/base/comm/HMTextChangeListener.java
@@ -0,0 +1,17 @@
+package com.hm.iou.base.comm;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+
+public abstract class HMTextChangeListener implements TextWatcher {
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+
+    }
+}
